c1b380da61c049ea41c29f7659edb971e94bb49c
emanuelnovaisr <emanuel@imdesenv.com>
2024-01-03 15:10:28 -0300

    buscando informações do s3

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,10 @@ import { Header } from '../../components/Header'
 import { Initial1366 } from '../../components/Initial/1366'
 import { Projects } from '../../components/ListProjects'
 import './styles.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Brandlab } from '../../components/Brandlab'
 import { About } from '../../components/About'
 import { Contact } from '../../components/Contact'
-import { useQuery } from "graphql-hooks";
-import { HOME_QUERY } from './query'
 import Logo from '../../assets/logo-black.png'
 import SyncLoader from "react-spinners/SyncLoader"
 import { TransitionLogo } from '../../components/TransitionLogo'
@@ -20,35 +18,35 @@ import { Initial3840 } from '../../components/Initial/3840'
 import { Initial1440 } from '../../components/Initial/1440'
 import { Initial768 } from '../../components/Initial/768'
 
+import axios from 'axios'
+
 export const Home = () => {
   const [showHeader, setShowHeader] = useState(false)
-
   const [showAnimationStart, setShowAnimationStart] = useState(true)
-
-  const { loading, error, data } = useQuery(HOME_QUERY)
-
   const [headerFunction, setHeaderFunction] = useState(null)
 
-  const changeFunctionHeader = (functionHeader) =>{
-    setHeaderFunction(() => functionHeader)
-  }
-
-  let newData = data
-  let errorTotal = false
-
-  if ( data ) {
-    localStorage.setItem('data', JSON.stringify(data))
-  }
+  const [data, setData] = useState({})
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(true)
 
 
-  if (error ) {
-    let dataStorage = localStorage.getItem('data')
-    if(dataStorage){
-      newData = JSON.parse(dataStorage)
-      console.log(newData)
-    }else{
-      errorTotal = true
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(process.env.REACT_APP_URL_JSON)
+        setData(response.data)
+        setLoading(false)
+      } catch (error) {
+        console.error('Erro ao obter dados:', error)
+        setError(true)
+        setLoading(false)
+      }
     }
+    fetchData()
+  }, [])
+
+  const changeFunctionHeader = (functionHeader) => {
+    setHeaderFunction(() => functionHeader)
   }
 
   const showScroll = (value) => {
@@ -60,25 +58,25 @@ export const Home = () => {
   }
 
   const accptedWidths = [1280, 1366, 1440, 1600, 1920, 2560, 3840]
-  
+
   const { width } = useWindowSize()
 
   const isMobile = width <= 1000
 
   const newWidth = isMobile ? 'mobile' : accptedWidths.reduce((prev, curr) => {
-    return (curr > width && curr < prev || curr === width ) ? curr : prev
+    return (curr > width && curr < prev || curr === width) ? curr : prev
   }, 1920)
 
 
   const SIZES_RES = {
-    'mobile': <Initial768 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart}  showScroll={showScroll} data={newData?.initial} setShowHeader={setShowHeader} />,
-    1280: <Initial1280 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart}  showScroll={showScroll} data={newData?.initial} setShowHeader={setShowHeader} />,
-    1366: <Initial1366 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart}  showScroll={showScroll} data={newData?.initial} setShowHeader={setShowHeader} />,
-    1440: <Initial1440 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart}  showScroll={showScroll} data={newData?.initial} setShowHeader={setShowHeader} />,
-    1600: <Initial1600 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart}  showScroll={showScroll} data={newData?.initial} setShowHeader={setShowHeader} />,
-    1920: <Initial1920 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart}  showScroll={showScroll} data={newData?.initial} setShowHeader={setShowHeader} />,
-    2560: <Initial2560 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart}  showScroll={showScroll} data={newData?.initial} setShowHeader={setShowHeader} />,
-    3840: <Initial3840 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart}  showScroll={showScroll} data={newData?.initial} setShowHeader={setShowHeader} />,
+    'mobile': <Initial768 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart} showScroll={showScroll} data={data?.initial} setShowHeader={setShowHeader} />,
+    1280: <Initial1280 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart} showScroll={showScroll} data={data?.initial} setShowHeader={setShowHeader} />,
+    1366: <Initial1366 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart} showScroll={showScroll} data={data?.initial} setShowHeader={setShowHeader} />,
+    1440: <Initial1440 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart} showScroll={showScroll} data={data?.initial} setShowHeader={setShowHeader} />,
+    1600: <Initial1600 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart} showScroll={showScroll} data={data?.initial} setShowHeader={setShowHeader} />,
+    1920: <Initial1920 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart} showScroll={showScroll} data={data?.initial} setShowHeader={setShowHeader} />,
+    2560: <Initial2560 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart} showScroll={showScroll} data={data?.initial} setShowHeader={setShowHeader} />,
+    3840: <Initial3840 showAnimationStart={showAnimationStart} setShowAnimationStart={setShowAnimationStart} showScroll={showScroll} data={data?.initial} setShowHeader={setShowHeader} />,
   }
 
   return (
@@ -89,18 +87,18 @@ export const Home = () => {
           <SyncLoader className='spinner' color={'var(--color-loading)'} />
         </div>
       }
-      {!loading && !errorTotal &&
+      {!loading && !error &&
         <>
           {showHeader &&
-            <Header showScroll={showScroll} data={newData.headerImg} headerFunction={headerFunction}/>
+            <Header showScroll={showScroll} data={data.headerImg} headerFunction={headerFunction}/>
           }
           {SIZES_RES[newWidth]}
-          <About text={newData.about} />
-          <TransitionLogo id={1} data={newData.transitionOne} />
-          <Projects showScroll={showScroll} projects={newData.allProjects} changeFunctionHeader={changeFunctionHeader}/>
-          <Brandlab showScroll={showScroll} text={newData.brandlab} brands={newData.allBrands} />
-          <TransitionLogo id={2} data={newData.transitionTwo} />
-          <Contact contact={newData.contact} />
+          <About text={data.about} />
+          <TransitionLogo id={1} data={data.transitionOne} />
+          <Projects showScroll={showScroll} projects={data.allProjects} changeFunctionHeader={changeFunctionHeader}/>
+          <Brandlab showScroll={showScroll} text={data.brandlab} brands={data.allBrands} />
+          <TransitionLogo id={2} data={data.transitionTwo} />
+          <Contact contact={data.contact} />
         </>
       }
     </div>
