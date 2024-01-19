bbdfdee801e9613fdd5d5e5a57ef3cb058236a8f
emanuelnovaisr <emanuel@imdesenv.com>
2024-01-19 15:15:45 -0300

    ajuste toggle

--- src/context/Toggle.js
+++ src/context/Toggle.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import { useFeatureIsOn } from '@growthbook/growthbook-react';
 
 export default function ToggleProvider({ children }) {
+  const [isFeatureReady, setIsFeatureReady] = useState(false);
   const isPreview = useFeatureIsOn('hysteria-preview');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 10));
+      setIsFeatureReady(true);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!isFeatureReady) {
+    return children;
+  }
 
   if (isPreview) {
     return children;
